5faf8e86a50c17cdae3bebfa66878b6edc2a93c8
s <s@localhost.localdomain>
2023-05-14 20:02:14 +0700

    add hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "../constans/data.json";
 import Button from "./Button";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Update isOpen based on the viewport size
+      setIsOpen(window.innerWidth > 768); // Example breakpoint: 768px
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Call handleResize initially
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <div className="container mx-auto lg:flex lg:flex-wrap p-5 flex-col md:flex-row items-center justify-between">
           <span className="text-2xl leading-6 font-bold font-poppins text-cinnabar">
             Edu.dot
           </span>{" "}
-          <div className={`${isOpen ? "block" : "hidden"}`}></div>
           <nav
             className={`${
               isOpen ? "block" : "hidden"
@@ -27,40 +49,35 @@ export default function Navbar() {
                 </span>
               ))}
           </nav>
-          <button
+          {/* <button
             className={`${
               isOpen ? "block" : "hidden"
             }inline-flex items-center font-poppins border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base font-normal mt-4 mr-4 md:mt-0`}
           >
             Log in
           </button>
-          <Button>Sign up</Button>
+          <Button>Sign up</Button> */}
           <button
-            onClick={() => setIsOpen(!isOpen)}
+            onClick={handleToggle}
             className="block lg:hidden float-right rounded text-black-500 hover:text-black-400"
           >
-                     {" "}
-            <svg
-              className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-                         {" "}
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />         {" "}
-            </svg>
-                     {" "}
-            <svg
-              className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-                         {" "}
-              <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-                       {" "}
-            </svg>
-                   {" "}
+            {isOpen ? (
+              <FaTimes className="fill-current h-3 w-3" />
+            ) : (
+              <FaBars className="fill-current h-3 w-3" />
+            )}
           </button>
-          {/* mobile view */}       {" "}
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } inline-flex items-center font-poppins border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base font-normal mt-4 mr-4 md:mt-0 block`}
+          >
+            {" "}
+            <button className="font-poppins block border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base font-normal mt-4 mr-4 md:mt-0">
+              Log in
+            </button>
+            <Button>Sign up</Button>
+          </div>
         </div>
       </header>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "components/Navbar";
+import Hero from "components/Hero";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
